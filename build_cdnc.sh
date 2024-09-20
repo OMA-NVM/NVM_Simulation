d163b7dc5d64f6343b5ea96d8fa46b0a49b8c689
ali10129 <10778765+ali10129@users.noreply.github.com>
2024-09-20 15:55:35 +0200

    updating `build_cdnc.sh`

--- build_cdnc.sh
+++ build_cdnc.sh
@@ -19,10 +19,11 @@ echo -e "$BOLD_GREEN clonning $BOLD_YELLOW NVM_Simulation.git cdnc branch  $TEXT
 git clone -b cdnc --single-branch https://github.com/OMA-NVM/NVM_Simulation.git
 
 
-echo -e "$BOLD_GREEN pwd= $BOLD_YELLOW $main_dir  $TEXT_RST"
+cd NVM_Simulation/
+
+echo -e "$BOLD_GREEN pwd= $BOLD_YELLOW $main_dir/NVM_Simulation/  $TEXT_RST"
 echo -e "$BOLD_GREEN running $BOLD_YELLOW git submodule init...  $TEXT_RST"
 
-cd NVM_Simulation/
 git submodule init && git submodule update --remote --init
 
 echo -e "$BOLD_GREEN running $BOLD_YELLOW ./cdnc/skip-Steps1and2.sh $TEXT_RST"
@@ -33,4 +34,6 @@ echo -e "$BOLD_GREEN building $BOLD_YELLOW gem5.opt/X86 $TEXT_RST"
 
 ./cdnc/step3-testingWithGem5.sh -c
 
-echo -e "$BOLD_GREEN done $TEXT_RST"
+cd "$main_dir"
+echo -e "$BOLD_GREEN pwd= $BOLD_YELLOW $main_dir  $TEXT_RST"
+echo -e "$BOLD_GREEN Done :) $TEXT_RST"
