9694001cf27426b7a2a8babfc64e7e809f5d3364
ali10129 <10778765+ali10129@users.noreply.github.com>
2024-09-20 15:39:05 +0200

    adding `build_cdnc.sh` for ESweek container purpose

---
+++ build_cdnc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+########
+# this is for downloading and compiling all the necessary files before running the simulation
+########
+
+set -e # to stop execution if there is an error
+
+BOLD_GREEN="\t\e[1;32m==> "
+BOLD_YELLOW="\e[1;33m"
+TEXT_RST=" \e[1;32m <==\e[0m"
+
+main_dir="$(pwd)"  # run script outside of NVM_SIMULATION/
+
+
+echo -e "$BOLD_GREEN pwd= $BOLD_YELLOW $main_dir  $TEXT_RST"
+
+echo -e "$BOLD_GREEN clonning $BOLD_YELLOW NVM_Simulation.git cdnc branch  $TEXT_RST"
+git clone -b cdnc --single-branch https://github.com/OMA-NVM/NVM_Simulation.git
+
+
+echo -e "$BOLD_GREEN pwd= $BOLD_YELLOW $main_dir  $TEXT_RST"
+echo -e "$BOLD_GREEN running $BOLD_YELLOW git submodule init...  $TEXT_RST"
+
+cd NVM_Simulation/
+git submodule init && git submodule update --remote --init
+
+echo -e "$BOLD_GREEN running $BOLD_YELLOW ./cdnc/skip-Steps1and2.sh $TEXT_RST"
+
+./cdnc/skip-Steps1and2.sh 
+
+echo -e "$BOLD_GREEN building $BOLD_YELLOW gem5.opt/X86 $TEXT_RST"
+
+./cdnc/step3-testingWithGem5.sh -c
+
+echo -e "$BOLD_GREEN done $TEXT_RST"
